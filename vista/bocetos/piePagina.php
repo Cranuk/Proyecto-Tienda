5ec054912d0df9219ad40bfa9abd536d997c20d6
Cranuk <facundom4t0@gmail.com>
2023-12-17 00:14:15 -0300

    arreglo minimo al footer

--- vista/bocetos/piePagina.php
+++ vista/bocetos/piePagina.php
@@ -1,7 +1,7 @@
         </section>
         <!------------------ PIE DE PAGINA ------------------>
         <footer id="piePagina">
-            Desarrollado por Cranuk &copy; <?= date("Y") ?>
+            Cranuk System &copy; All rights reserved 2020-<?php echo date("Y"); ?>
         </footer>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
